d6c29238deb05480409343840ef1daa1c44f2136
Kazuhide Oki <cedar123pc@gmail.com>
2020-04-07 17:24:23 +0800

    同

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 const express = require("express");
 const next = require("next");
+const mysql = require("mysql")
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
@@ -10,11 +11,51 @@ app
   .prepare()
   .then(() => {
 
-    //   -----------ここにバックエンドの処理を書く-----------
+    server.get("/post_data", (req, res) => {
+        console.log('post_dataだよ');
+        const connection = mysql.createConnection({
+          host: "localhost",
+          user: "root",
+          password: "root",
+          database: "salon_tablet",
+        });
+        
+      connection.connect(function (err) {
+        if (err) throw err;
+
+        connection.query("select title from post_data", function (
+          err,
+          result,
+          fields
+        ) {
+          if (err) throw err;
+          console.log('resultは' + result);
+
+          return res.send(result[0]);
+        });
+
+        // return handler(req, res);
+      });
+    });
+
+
+
+    server.get("/test", (req, res) => {
+        console.log('serverのtestだよ');
+        
+        return res.send("testさん");
+    });
 
     server.get("*", (req, res) => {
+      console.log("nextjsのルーティング");
+
       return handler(req, res);
     });
+
+
+    //   -----------ここにバックエンドの処理を書く-----------
+
+    
 
     server.listen(3000, (err) => {
       if (err) console.error(err.stack);
---
+++ src/pages/testData.jsx
@@ -0,0 +1,44 @@
+// import fetch from "isomorphic-unfetch";
+import fetch from "node-fetch";
+// const http = require("http");
+import useSWR from "swr";
+const mysql = require("mysql");
+
+
+
+const TestData = (props) => {
+    console.log('propsは'  + props);
+    
+
+    //     const { data, error } = await useSWR("http://localhost:10080/test");
+    //   console.log('返り値は' + data);
+
+    // const res = fetch("http://localhost:10080/test", {method: 'GET'});
+    // const text = res.text();
+
+
+    
+    return (
+    <div>
+       {props.data.title} 
+       {/* {text}  */}
+       これはただの文章d
+    </div>
+    )
+}
+
+
+// TestData.getServerSideProps = async () => {
+// export async function getStaticProps() {
+export async function getServerSideProps() {
+    const res = await fetch("http://localhost:3000/post_data", {method: 'GET'});
+    const data = await res.json();
+    console.log('initialは' + data);
+    
+    return {props: {data}};
+
+
+
+};
+
+export default TestData;
