fe1d82a060fe53844e012ee776097d6f02eae0e6
Kazuhide Oki <cedar123pc@gmail.com>
2020-04-08 08:51:40 +0800

    testData.jsxをととのえた

--- src/pages/testData.jsx
+++ src/pages/testData.jsx
@@ -1,22 +1,7 @@
-// import fetch from "isomorphic-unfetch";
 import fetch from "node-fetch";
-// const http = require("http");
-import useSWR from "swr";
-// const mysql = require("mysql");
-
-
 
 const TestData = (props) => {
     console.log('propsは'  + props);
-    
-
-    //     const { data, error } = await useSWR("http://localhost:10080/test");
-    //   console.log('返り値は' + data);
-
-    // const res = fetch("http://localhost:10080/test", {method: 'GET'});
-    // const text = res.text();
-
-
     
     return (
     <div>
@@ -27,18 +12,12 @@ const TestData = (props) => {
     )
 }
 
-
-// TestData.getServerSideProps = async () => {
-// export async function getStaticProps() {
 export async function getServerSideProps() {
     const res = await fetch("http://localhost:3000/post_data", {method: 'GET'});
-    // const res = await http.get("http://localhost:3000/post_data");
     const data = await res.json();
     console.log('initialは' + data);
     
     return {props: {data}};
-
-
 
 };
 
