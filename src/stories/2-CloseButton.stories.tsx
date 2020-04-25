b8bc87399b6dcdccfb8726abe877a48526f7ed62
Kazuhide Oki <cedar123pc@gmail.com>
2020-04-25 23:31:15 +0800

    storybook導入完了

---
+++ src/stories/2-CloseButton.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { CloseButton } from "../app/PModal/CloseButton";
+export default {
+  title: "CloseButton",
+  component: CloseButton,
+};
+
+
+
+export const Normal = () => (
+  <CloseButton/>
+);
