390e3002668d1e40379687b0627263c2db0c3e04
Kazuhide Oki <cedar123pc@gmail.com>
2020-04-26 13:35:42 +0800

    Merge commit 'b56df5fd97c5ea6634d0559597406bf41867a6d4'

---
+++ src/stories/CloseButton.stories.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { CloseButton } from "../app/PModal/CloseButton";
+export default {
+  title: "CloseButton",
+  component: CloseButton,
+};
+
+export const Normal = () => (
+  <CloseButton/>
+);
