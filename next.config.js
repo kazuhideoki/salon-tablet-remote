93475175587f183b287696def6a27b9bfcab42fb
Kazuhide Oki <cedar123pc@gmail.com>
2020-05-04 14:57:30 +0800

    build時に static/service-worker.js" に作られるように

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 // 【PWA対応】withOffline を読み込む
 // next.config.js
 var withOffline = require('next-offline')
+
+var nextConfig = {
+  dontAutoRegisterSw: true,
+  workboxOpts: { swDest: "static/service-worker.js" },
+};
  
-module.exports = withOffline({ dontAutoRegisterSw: true })
+module.exports = withOffline(nextConfig)
 
 
 
