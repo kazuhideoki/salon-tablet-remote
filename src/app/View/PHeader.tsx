d3d87ebf29c6d99dead7523a87d125c420ad1ca5
Kazuhide Oki <cedar123pc@gmail.com>
2020-05-23 08:02:29 +0800

    整形

--- src/app/View/PHeader.tsx
+++ src/app/View/PHeader.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { Typography, makeStyles, createStyles, Theme } from '@material-ui/core'
 
 
+
+const useStyles = makeStyles((theme: Theme) => {
+  return createStyles({
+    root: {
+      height: "100%",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+    }
+  })
+})
 
 export const PHeader = () => {
   return (
     <div>
-      ヘッダー
+      <Typography align="center" variant="h5">
+        Salon Tablet
+      </Typography>
     </div>
   )
 }
---
+++ src/stories/PHeader.stories.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { PHeader } from '../app/View/PHeader';
+export default {
+title: 'PHeader',
+component: PHeader,
+};
+export const Normal = () => <PHeader />;
