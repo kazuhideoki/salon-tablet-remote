5a8ea117dd83d7b8d43852eecbed36c6e72b0dc4
Kazuhide Oki <cedar123pc@gmail.com>
2020-06-17 07:19:59 +0800

    パスワード未入力の場合は更新しない

--- src/pages/api/user_info/update.ts
+++ src/pages/api/user_info/update.ts
@@ -3,28 +3,27 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { T_update_user } from "../../../app/ActionCreator/user/useUpdateUser";
 import { cipher } from "../../../module/bcrypt";
 
-
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
-    const { columns }: T_update_user = req.body;
-    
-    // sqlに入れる用のparamsを,bcryptとともに生成し直す
-    // const params = Object.assign({ bcrypt_password: '' }, columns);
-    const bcrypt = cipher(req.body.plainTextPassword);
-    console.log(bcrypt);
-    //@ts-ignore
-    // params.bcrypt_password = bcrypt;
-    
-    const params = {
-      bcrypt_password: bcrypt,
-      user_id: columns.user_id,
-      user_name: columns.user_name,
-      shop_name: columns.shop_name,
-      user_email: columns.user_email,
+    const { columns, plainTextPassword }: T_update_user = req.body;
+
+    let params
+    if (plainTextPassword === "") {
+      params = columns
+
+    } else {
+      // sqlに入れる用のparamsを,bcryptとともに生成し直す
+      const bcrypt = cipher(plainTextPassword);    
+      params = {
+        bcrypt_password: bcrypt,
+        user_id: columns.user_id,
+        user_name: columns.user_name,
+        shop_name: columns.shop_name,
+        user_email: columns.user_email,
+      }
+
     }
-  
-    console.log(JSON.stringify(params));
-    
+        
     console.log("/user_info/update/のsqlに入れるparamsは " + JSON.stringify(params));
 
     try {
