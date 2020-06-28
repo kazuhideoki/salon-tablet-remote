5b470c534305814a3c8037c0ce5fa6ef3a24c27b
Kazuhide Oki <cedar123pc@gmail.com>
2020-06-28 08:36:01 +0800

    HelpButton完成

---
+++ src/app/View/buttons/HelpButton.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import Popover from "@material-ui/core/Popover";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import { IconButton } from "@material-ui/core";
+import { Help } from "@material-ui/icons";
+import { type } from "os";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    typography: {
+      padding: theme.spacing(2),
+    },
+  })
+);
+type THelpButton = {className?: string, content: string}
+
+export const HelpButton: React.FC<THelpButton> = ({ className, content }) => {
+         const classes = useStyles();
+         const [
+           anchorEl,
+           setAnchorEl,
+         ] = React.useState<HTMLButtonElement | null>(null);
+
+         const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+           setAnchorEl(event.currentTarget);
+         };
+
+         const handleClose = () => {
+           setAnchorEl(null);
+         };
+
+         const open = Boolean(anchorEl);
+         const id = open ? "simple-popover" : undefined;
+
+         return (
+           <div className={className}>
+             <IconButton
+               aria-label="help"
+               aria-describedby={id}
+               // variant="contained"
+               // color="primary"
+               onClick={handleClick}
+             >
+               <Help />
+             </IconButton>
+             <Popover
+               id={id}
+               open={open}
+               anchorEl={anchorEl}
+               onClose={handleClose}
+               anchorOrigin={{
+                 vertical: "top",
+                 horizontal: "center",
+               }}
+               transformOrigin={{
+                 vertical: "bottom",
+                 horizontal: "center",
+               }}
+             >
+               <Typography className={classes.typography}>{content}</Typography>
+             </Popover>
+           </div>
+         );
+       };
---
+++ src/stories/HelpButton.stories.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { HelpButton } from "../app/View/buttons/HelpButton";
+import classes from "*.module.css";
+import { makeStyles, createStyles } from "@material-ui/core";
+export default {
+  title: "HelpButton",
+  component: HelpButton,
+};
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      position: "relative",
+      width: "100vw",
+      height: "100vh",
+    },
+    button: {
+      position: "absolute",
+      left: 0,
+      right: 0,
+      top: 0,
+      bottom: 0,
+      margin: "auto",
+    },
+  })
+);
+export const Normal = () => {
+  return (
+    <>
+      <p>tesuto</p>
+      <p>tesuto</p>
+      <p>tesuto</p>
+      <p>tesuto</p>
+      <p>tesuto</p>
+      <p>tesuto</p>
+      <p>tesuto</p>
+      <HelpButton content="中身の文章"/>
+    </>
+  );
+};
+export const Center = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <HelpButton className={classes.button} content={"中央に配置できたかな？"}/>
+    </div>
+  );
+};
