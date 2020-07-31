2c200da341a08d6193099580b160a976c9831a6c
Kazuhide Oki <cedar123pc@gmail.com>
2020-07-31 11:40:19 +0800

    httpsでlocalhostの立ち上げ成功

--- src/app/View/Drawer/ManageInstagmaAccounts.tsx
+++ src/app/View/Drawer/ManageInstagmaAccounts.tsx
@@ -20,34 +20,17 @@ const useStyles = makeStyles((theme: Theme) =>
 export const ManageInstagmaAccounts = () => {
   const classes = useStyles()
 
-  const handleResisterInstagram = () => {
-    // const res = await fetch(
-    //   `https://${server}/instagram_accounts/connect_short`
-    // );
-    fetch(
-      `https://api.instagram.com/oauth/authorize?client_id=298521954536312&redirect_uri=${server}/api/get_token/&scope=user_profile,user_media&response_type=code`
-    );
-
-  }
-
-  // https://api.instagram.com/oauth/authorize?client_id=298521954536312&redirect_uri=https://localhost:3000/api/instagram_accounts/get_token/&scope=user_profile,user_media&response_type=code
-
   const instaAuth = `https://api.instagram.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_INSTAGRAM_CLIENT_ID}&redirect_uri=${instagramRedirectHost}/api/instagram_accounts/get_token&scope=user_profile,user_media&response_type=code`;
 
   return (
     <div>
       <Typography variant="h4" component="h2" className={classes.header}>
-        タグ管理
+        Instagram アカウント管理
       </Typography>
-      {/* <Button onClick={() => handleResisterInstagram()}>
-        インスタグラムアカウントを登録する
-      </Button> */}
-      <Button>
-        {/* <Link href="https://api.instagram.com/oauth/authorize?client_id=298521954536312&redirect_uri=${server}/api/instagram_accounts/get_token/&scope=user_profile,user_media&response_type=code" >
-          インスタグラムアカウントを登録する
-        </Link> */}
-        <a href={instaAuth}>インスタグラムアカウントを登録する</a>
-      </Button>
+
+      <a href={instaAuth}>
+        <Button>インスタグラムアカウントを登録する</Button>
+      </a>
     </div>
   );
 }
