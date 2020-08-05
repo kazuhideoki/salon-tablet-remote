14cddbf0878bb520ac64bcb3ee515701cbe7e51f
Kazuhide Oki <cedar123pc@gmail.com>
2020-08-05 08:31:03 +0800

    日付表示のバグ修正

--- src/app/ActionCreator/organizeSql/sqlToDate.ts
+++ src/app/ActionCreator/organizeSql/sqlToDate.ts
@@ -1,17 +1,16 @@
 // データベースからフロントに渡ってきたデータを整形する
-export const sqlToDate = (arg) => {
-    const date = new Date(arg);
+export const sqlToDate = (arg: string) => {
+  
+  // safariでのNaNになってしまうバグを防ぐため+を.に変換
+  var date0 = arg.replace("+", ".")
+  const date = new Date(date0);
 
-    return (
-      (date.getMonth() +
-      1) +
-      "月" +
-      date.getDate() +
-      "日"
-      // +
-      // date.getHours() +
-      // "時" +
-      // date.getMinutes() +
-      // "分"
-    );
-}
+  return (
+    date.getMonth() + 1 + "月" + date.getDate() + "日"
+    // +
+    // date.getHours() +
+    // "時" +
+    // date.getMinutes() +
+    // "分"
+  );
+};
