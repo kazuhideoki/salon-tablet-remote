63abc6ca8d722cdb75d22e398e5718ce2f149d46
Kazuhide Oki <cedar123pc@gmail.com>
2020-08-18 10:35:18 +0800

    devで動作確認

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,8 @@
 const dev = process.env.NODE_ENV !== "production";
 
-const port =
-  process.env.TEST === "true"
+const test = process.env.NEXT_PUBLIC_TEST === "true";
+
+const port = test
     ? process.env.NEXT_PUBLIC_PORT_TEST
     : process.env.NEXT_PUBLIC_PORT;
 
@@ -9,20 +10,24 @@ const port =
     
 // HTTPSは "npm run https" のときに env変数として "https://localhoset:3000"で設定
 // サーバーサイドでapi通信をするときに、sslの有無で切り替える
-export const localhost =
-         process.env.NEXT_PUBLIC_SITE_DEV_HTTPS + port ||
-         process.env.NEXT_PUBLIC_SITE_DEV + port
+export const localhost = process.env.NEXT_PUBLIC_SITE_DEV_HTTPS
+         ? process.env.NEXT_PUBLIC_SITE_DEV_HTTPS + port
+         : process.env.NEXT_PUBLIC_SITE_DEV + port
 // export const localhost = HTTPS || process.env.NEXT_PUBLIC_SITE_DEV ;
 
 // フロント側でapiにfetchする時
 export const server = dev
-         ? process.env.NEXT_PUBLIC_SITE_DEV_HTTPS + port ||
-           process.env.NEXT_PUBLIC_SITE_DEV + port
+         ? process.env.NEXT_PUBLIC_SITE_DEV_HTTPS
+           ? process.env.NEXT_PUBLIC_SITE_DEV_HTTPS + port
+           : process.env.NEXT_PUBLIC_SITE_DEV + port
            
-         : process.env.NEXT_PUBLIC_SITE;
+         : test
+           ? process.env.NEXT_PUBLIC_SITE
+           : process.env.NEXT_PUBLIC_SITE_TEST;
 
 // instagram apiでは、redirect uriにdev環境でもhttpsが必要。
 export const instagramRedirectHost = dev
          ? process.env.NEXT_PUBLIC_SITE_DEV_HTTPS + port
-         : process.env.NEXT_PUBLIC_SITE;
-
+         : test
+           ? process.env.NEXT_PUBLIC_SITE
+           : process.env.NEXT_PUBLIC_SITE_TEST;
