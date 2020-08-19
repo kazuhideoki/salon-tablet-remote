c0404e528aee0bd31f3865aa85a2680e6364fe7f
Kazuhide Oki <cedar123pc@gmail.com>
2020-08-19 08:46:33 +0800

    .envを環境ごとに分けることに

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,44 +1,22 @@
 const dev = process.env.NODE_ENV !== "production";
-
-const test = process.env.NEXT_PUBLIC_TEST === "true";
-
-const port = test
-    ? process.env.NEXT_PUBLIC_PORT_TEST
-    : process.env.NEXT_PUBLIC_PORT;
-
-
     
-// HTTPSは "npm run https" のときに env変数として "https://localhost:3000"で設定
+// HTTPSは "npm run https" 起動のときに env変数として SSL"https://localhost:3000"で設定
+
 // サーバーサイドでapi通信をするときに、sslの有無で切り替える
-export const localhost = process.env.NEXT_PUBLIC_SITE_DEV_HTTPS
-         ? process.env.NEXT_PUBLIC_SITE_DEV_HTTPS + port
-         : process.env.NEXT_PUBLIC_SITE_DEV + port
-// export const localhost = HTTPS || process.env.NEXT_PUBLIC_SITE_DEV ;
+export const localhost = process.env.HTTPS || process.env.LOCALHOST 
 
 // フロント側でapiにfetchする時
-export const server = dev
-         ? process.env.NEXT_PUBLIC_SITE_DEV_HTTPS
-           ? process.env.NEXT_PUBLIC_SITE_DEV_HTTPS + port
-           : process.env.NEXT_PUBLIC_SITE_DEV + port
-           
-         : test
-           ? process.env.NEXT_PUBLIC_SITE_TEST
-           : process.env.NEXT_PUBLIC_SITE;
+// ※"npm run https"起動で serverではサーバー側で動作するが、client側では読み込まれない...→最初の読み込みに必要
+export const server = process.env.HTTPS || process.env.NEXT_PUBLIC_SITE;
 
 // instagram apiでは、redirect uriにdev環境でもhttpsが必要。
 export const instagramRedirectHost = dev
-         ? process.env.NEXT_PUBLIC_SITE_DEV_HTTPS + port
-         : test
-           ? process.env.NEXT_PUBLIC_SITE_TEST
-           : process.env.NEXT_PUBLIC_SITE;
+         ? "https://localhost:3000"
+         : process.env.NEXT_PUBLIC_SITE;
 
 console.log(
     "devは " +
     dev +
-    " testは " +
-    test +
-    " portは " +
-    port +
     " localhostは " +
     localhost +
     " serverは " +
