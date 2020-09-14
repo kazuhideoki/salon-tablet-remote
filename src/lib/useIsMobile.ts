a110c9b4a73d62aabc549f08887c2603eb112bc1
Kazuhide Oki <cedar123pc@gmail.com>
2020-09-14 16:12:40 +0800

    useIsMobileの修正、初期描画とレスポンシブ対応

--- src/lib/useIsMobile.ts
+++ src/lib/useIsMobile.ts
@@ -5,46 +5,37 @@ import { Store } from '../app/Store/Store';
 export const useIsMobile = () => {
   const { appState } = React.useContext(Store)
   const { uaDevice, selectedDevice } = appState
-  const smallWidth = useMediaQuery("(max-width:480px)");
-  // const smallWidth = useMediaQuery("(max-width:480px)", {
-  //   defaultMatches: props.window().matchMedia,
-  // });
-  const smallHeight = useMediaQuery("(max-height:480px)");
-  // const smallHeight = useMediaQuery("(max-height:480px)", {
-  //   matchMedia: props.window().matchMedia,
-  // });
+
+  // 描画直後の判定に（直後はmobileでもfalseになるため）
+  const defaultMatches = () => {
+    if (uaDevice === "mobile" || uaDevice === "wearable") {
+      // console.log("★mobile");
+      return true;
+    } else {
+      // console.log("★tablet");
+      return false;
+    }
+  }
+
+  const smallWidth = useMediaQuery("(max-width:480px)", {
+    defaultMatches: defaultMatches()
+  });
+  // console.log(smallWidth);
+  const smallHeight = useMediaQuery("(max-height:480px)", {
+    defaultMatches: defaultMatches(),
+  });
+  // console.log(smallHeight);
+
+  
 
   if (selectedDevice === "mobile") {
     console.log('selectedDevice === "mobile"');
-
     return true;
   }
   if (selectedDevice === "tablet") {
     console.log('selectedDevice === "tablet"');
     return false;
   }
-
-
-  // SSRで初期表示させるときの処理 + フロントの最初の描画ではmediaQuerryの情報がないのでuaDeviceをもとに描画
-  if (process.browser === false || !smallWidth || !smallHeight) {
-    if (uaDevice === "mobile" || uaDevice === "wearable") {
-      console.log("★mobile");
-      return true;
-    } else {
-      console.log("★tablet");
-      return false;
-    }
-  }
-
-  // if (selectedDevice === 'mobile') {
-  //   console.log('selectedDevice === "mobile"');
-    
-  //   return true
-  // }
-  // if (selectedDevice === 'tablet') {
-  //   console.log('selectedDevice === "tablet"');
-  //   return false
-  // }
 
   if (smallWidth || smallHeight) {
     console.log("(smallWidth || smallHeight)");
