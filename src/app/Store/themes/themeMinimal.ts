4a244c01a7c63abd29d7bec528b6833b0cb1e48d
Kazuhide Oki <cedar123pc@gmail.com>
2020-09-26 06:58:22 +0800

    buildエラー修正 +

--- src/app/Store/themes/themeMinimal.ts
+++ src/app/Store/themes/themeMinimal.ts
@@ -1,83 +1,83 @@
 import { createMuiTheme } from "@material-ui/core"
 import { TThemeParams } from "../ThemeContext"
-import { FontNameToFontFamily } from "./fonts"
+import { FontNameToFontFamily } from "./fonts";
 
 const theme = createMuiTheme()
 
-export const themeMinimal = (params: TThemeParams) => createMuiTheme({
-  overrides: {
-    MuiCssBaseline: {
-      "@global": {
-        a: {
-          color: "#134e78",
-        },
-        h1: {
-          fontWeight: "400",
-          borderBottom: "solid 3px black",
-        },
-        h2: {
-          fontWeight: "400",
-          padding: "0.25em 0.5em",/*上下 左右の余白*/
-          background: "transparent",/*背景透明に*/
-          borderLeft: "solid 5px",/*左線*/
-        },
-        
-      },
-    },
-    MuiPaper: {
-      rounded: {
-        borderRadius: theme.spacing(3),
-      },
-    },
-    MuiDrawer: {
-      paper: {
-        MozBorderRadiusTopright: theme.spacing(3),
-        MozBorderRadiusBottomright: theme.spacing(3),
-      }
-    },
-  },
+export const themeMinimal = (params: TThemeParams) =>
+         createMuiTheme({
+           overrides: {
+             MuiCssBaseline: {
+               "@global": {
+                 a: {
+                   color: "#134e78",
+                 },
+                 h1: {
+                   fontWeight: "400",
+                   borderBottom: "solid 3px black",
+                 },
+                 h2: {
+                   fontWeight: "400",
+                   padding: "0.25em 0.5em" /*上下 左右の余白*/,
+                   background: "transparent" /*背景透明に*/,
+                   borderLeft: "solid 5px" /*左線*/,
+                 },
+               },
+             },
+             MuiPaper: {
+               rounded: {
+                 borderRadius: theme.spacing(3),
+               },
+             },
+             MuiDrawer: {
+               paper: {
+                 MozBorderRadiusTopright: theme.spacing(3),
+                 MozBorderRadiusBottomright: theme.spacing(3),
+               },
+             },
+           },
 
-  typography: {
-    fontFamily: [
-      // "futura-pt",
-      FontNameToFontFamily(params.theme_font1),
-      // '"ヒラギノ角ゴ ProN"',
-      FontNameToFontFamily(params.theme_font2),
-      "-apple-system",
-      "BlinkMacSystemFont",
-      '"Segoe UI"',
-      "Roboto",
-      '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(","),
-  },
-   palette: {
-    primary: {
-      main: params.theme_color,
-    },
-    secondary: {
-      main: '#b0c4de', // lightsteelblue
-    },
-    // tonalOffset: 0.2,
-  },
+           typography: {
+             fontFamily: [
+               // "未設定",
+               FontNameToFontFamily(params.theme_font1),
+               // '"M PLUS Rounded 1c"',
+               FontNameToFontFamily(params.theme_font2),
+              //  "-apple-system",
+              //  "BlinkMacSystemFont",
+              //  '"Segoe UI"',
+              //  "Roboto",
+              //  '"Helvetica Neue"',
+              //  "Arial",
+               "sans-serif",
+              //  '"Apple Color Emoji"',
+              //  '"Segoe UI Emoji"',
+              //  '"Segoe UI Symbol"',
+             ].join(","),
+           },
+           palette: {
+             primary: {
+               main: params.theme_color,
+             },
+             secondary: {
+               main: "#b0c4de", // lightsteelblue
+             },
+             // tonalOffset: 0.2,
+           },
 
-  props: {
-    MuiPaper: {
-      variant: "elevation",
-      elevation: 3,
-    },
-    MuiTextField: {
-      variant: "standard",
-    },
-    MuiButton: {
-      variant: "outlined",
-    },
-    MuiFormControl: {
-      variant: "standard",
-    }
-  }
-})
+           props: {
+             MuiPaper: {
+               variant: "elevation",
+               elevation: 3,
+             },
+             MuiTextField: {
+               variant: "standard",
+             },
+             MuiButton: {
+               variant: "outlined",
+             },
+             MuiFormControl: {
+               variant: "standard",
+             },
+           },
+         });
