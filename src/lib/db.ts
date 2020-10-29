879bbe04799bb7c36eba9a493c6079e1ca5d3ad3
Kazuhide Oki <cedar123pc@gmail.com>
2020-10-29 10:39:58 +0800

    logつけて 値見る

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,17 +1,19 @@
 import serverlessMysql from "serverless-mysql";
 
-const mysql = serverlessMysql({
+const config = {
   config: {
-    
     host: process.env.NEXT_PUBLIC_MYSQL_HOST,
     database: process.env.NEXT_PUBLIC_MYSQL_DATABASE,
     user: process.env.NEXT_PUBLIC_MYSQL_USER,
     password: process.env.NEXT_PUBLIC_MYSQL_PASSWORD,
   },
-});
+};
+const mysql = serverlessMysql();
 
 // ※db(``)の返り値は常に[]
 export const db = async (query, params?) => {
+
+    console.log('mysqlのconfigは ' + config)
  
     const results = await mysql.query(query, params);
     await mysql.end();
