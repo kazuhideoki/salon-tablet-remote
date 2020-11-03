b21d4706e1df0a79f9696275b14293bc8f4120d6
Kazuhide Oki <cedar123pc@gmail.com>
2020-11-03 23:01:12 +0800

    devでapi/~でデータ変更可能

--- src/lib/loadUrl.ts
+++ src/lib/loadUrl.ts
@@ -1,9 +1,8 @@
 const dev = process.env.NODE_ENV !== "production";
     
-// HTTPSは "npm run https" 起動のときに env変数として SSL"https://localhost:3000"で設定
-
 // サーバーサイドでapi通信をするときに、sslの有無で切り替える
-export const localhost = process.env.HTTPS || process.env.LOCALHOST 
+// export const localhost = process.env.HTTPS || process.env.LOCALHOST 
+export const localhost = "http://localhost:8080";
 
 // フロント側でapiにfetchする時
 // ※"npm run https"起動で serverではサーバー側で動作するが、client側では読み込まれない...→最初の読み込みに必要
