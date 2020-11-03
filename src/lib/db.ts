20b26037bc157e84cd7ab34f5970298133503bea
Kazuhide Oki <cedar123pc@gmail.com>
2020-11-03 21:19:59 +0800

    修正

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,36 +1,36 @@
 import serverlessMysql from "serverless-mysql";
 
-const config = {
-  config: {
-    // host: process.env.NEXT_PUBLIC_MYSQL_HOST,
-    // database: process.env.NEXT_PUBLIC_MYSQL_DATABASE,
-    // user: process.env.NEXT_PUBLIC_MYSQL_USER,
-    // password: process.env.NEXT_PUBLIC_MYSQL_PASSWORD,
-    // // socketPath: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
-    // socketPath: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
-    host: 'localhost',
-    database: 'test_salon_tablet',
-    user: 'root',
-    password: 'root',
-    port: 3306,
-    // socketPath: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
-    // socketPath: "salon-tablet-294401:asia-northeast1:salon-tablet",
-  },
-};
-const configDev = {
-  config: {
-    host: "localhost",
-    database: "test_salon_tablet",
-    user: "root",
-    password: "root",
-    port: 3306,
-  },
-};
-
-const mysql =
+const config =
   process.env.NODE_ENV === "production"
-    ? serverlessMysql(config)
-    : serverlessMysql(configDev);
+    ? {
+        config: {
+          // host: process.env.NEXT_PUBLIC_MYSQL_HOST,
+          // database: process.env.NEXT_PUBLIC_MYSQL_DATABASE,
+          // user: process.env.NEXT_PUBLIC_MYSQL_USER,
+          // password: process.env.NEXT_PUBLIC_MYSQL_PASSWORD,
+          // // socketPath: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
+          // socketPath: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
+          host: 'localhost',
+          // host: "34.84.102.102",
+          database: "test_salon_tablet",
+          user: "root",
+          password: "root",
+          // port: 3306,
+          // socketPath: `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`,
+          socketPath: "/cloudsql/salon-tablet-294401:asia-northeast1:salon-tablet",
+        },
+      }
+    : {
+        config: {
+          host: "localhost",
+          database: "test_salon_tablet",
+          user: "root",
+          password: "root",
+          port: 3306,
+        },
+      };
+
+const mysql = serverlessMysql(config)
 
 // ※db(``)の返り値は常に[]
 export const db = async (query, params?) => {
--- src/server/serverHttps.ts
+++ src/server/serverHttps.ts
@@ -2,8 +2,10 @@
 // そのためにcustom serverを作って https化
 // npm run httpsで起動できる。
 
+
 const { createServer } = require("https");
 const { parse } = require("url");
+const { readFileSync } = require("fs");
 import next from "next";
 
 const port = process.env.NEXT_PUBLIC_PORT;
@@ -11,8 +13,18 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+// httpsでlocalhostにアクセスしたときの鍵認証エラーの回避 next-auth用
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = '0';
+
+// https接続にするためmkcertを使って鍵を作る (参考) https://kifarunix.com/how-to-create-self-signed-ssl-certificate-with-mkcert-on-ubuntu-18-04/
+const httpsOptions = {
+  key: readFileSync("./src/server/local.dev+4-key.pem"),
+  cert: readFileSync("./src/server/local.dev+4.pem"),
+  ca: [readFileSync("./src/server/rootCA.pem")],
+};
+
 app.prepare().then(() => {
-  createServer((req, res) => {
+  createServer(httpsOptions, (req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
   }).listen(port, (err) => {
