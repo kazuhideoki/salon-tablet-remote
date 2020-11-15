5939f6b7db708915d08d9b7c525aad198f450c74
Kazuhide Oki <cedar123pc@gmail.com>
2020-11-16 06:56:33 +0800

    修正

--- src/lib/auth/sendVerificationMail.ts
+++ src/lib/auth/sendVerificationMail.ts
@@ -3,7 +3,8 @@ export const sendVerificationMail = (user) => {
   user
     .sendEmailVerification()
     .then(function() {
-      // apiUserInfoEmailVerified()
+    
+
       return true
     })
     .catch(function(error) {
