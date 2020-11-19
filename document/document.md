6176df7a1d2a19e15e42d6cbbe24e6d7e65b8f9a
Kazuhide Oki <cedar123pc@gmail.com>
2020-11-19 08:31:39 +0800

    アイコン画像の変更

--- document/document.md
+++ document/document.md
@@ -2,10 +2,10 @@
 
 1. vagrant up
 2. vagrant ssh (password: vagrant)
-3. cd salon-tablet
+3. cd salon-tablet/proxy
 4. proxy通す (最初にダウンロード,設定する https://cloud.google.com/sql/docs/mysql/connect-admin-proxy?authuser=2#tcp-sockets_1)
 
-   `./cloud_sql_proxy -instances=salon-tablet-2:asia-northeast1:salon-tablet-db=tcp:3306`
+   `./mac_cloud_sql_proxy -instances=salon-tablet-2:asia-northeast1:salon-tablet-db=tcp:3306`
 5. 別のターミナルウィンドウから `npm run dev`
 
   <!-- 本番?： ./../cloud_sql_proxy -instances=salon-tablet-2:asia-northeast1:salon-tablet-db=tcp:3306 \ -credential_file=~/.ssh/salon-tablet-2-da074a755124.json & -->
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -38,24 +38,6 @@ class MyDocument extends Document {
           <meta name="msapplication-TileColor" content="#2B5797" />
           <meta name="msapplication-tap-highlight" content="no" />
           <meta name="theme-color" content="#000000" />
-
-          {/* <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/static/icons/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/static/icons/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/static/icons/favicon-16x16.png"
-          /> */}
           <link
             rel="apple-touch-icon"
             type="image/png"
