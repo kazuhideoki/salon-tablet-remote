db0e28975fc8191c50e66895f0181648c06d459e
Kazuhide Oki <cedar123pc@gmail.com>
2021-01-01 08:24:57 +0800

    public page表示切り替え可能に

--- src/lib/checkIsGeneratePubulicPage.ts
+++ src/lib/checkIsGeneratePubulicPage.ts
@@ -1,23 +1,22 @@
-import { db } from "./db";
 import { TUserInfo } from "../app/Store/Interface";
+import { db } from "./db";
 import { userInfoParamsFromSql } from "./userInfoParamsFromSql";
 
 // サーバーサイドとフロントサイド考えずに使えるようにラップする
-export const getUserInfoFromSlug = async (
+export const checkIsGeneratePubulicPage = async (
          slug: string
        ): Promise<TUserInfo> => {
          //@ts-ignore
          const data: TUserInfo[] = await db(`SELECT * FROM user_info`);
 
          const target = data.filter((value) => {
-           return value.public_page_slug === slug;
+           return (value.public_page_slug === slug && value.is_generate_public_page);
          });
 
          if (target.length) {
-          return userInfoParamsFromSql(target[0]);
+           return userInfoParamsFromSql(target[0]);
          } else {
-          return null
+           return null;
          }
-
        };
 
--- src/pages/public_page/[public_page_slug].tsx
+++ src/pages/public_page/[public_page_slug].tsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import { GetServerSideProps } from "next";
-import { getUserInfoFromSlug } from "../../lib/getUserInfoFromSlug";
 import { generateProps } from '../../lib/generateProps';
 import App from '../../app/View/App';
 import Head from 'next/head';
 import parser from "ua-parser-js";
 import { IndexProps } from '..';
+import { checkIsGeneratePubulicPage } from '../../lib/checkIsGeneratePubulicPage';
 
 
 
 const PublicPage = (props: IndexProps) => {
+
+  if (props.isPublicPage === false) {
+    return (<>
+      <Head>
+        <meta name="robots" content="noindex" />
+      </Head>
+      <p>パブリックページが有効化されていません</p>
+    </>)
+  }
 
   return (
     <>
@@ -32,16 +41,16 @@ export const getServerSideProps: GetServerSideProps = async ({req, res, query}) 
   const device = ua.getDevice().type;
   
   // slugがDBにあるかどうかチェックして、「表示させているか？」「slug」を返す
-  const userInfo = await getUserInfoFromSlug(SlugArray[0]);
+  const userInfo = await checkIsGeneratePubulicPage(SlugArray[0]);
 
   if (userInfo === null) {
-    res.statusCode = 302;
-    res.setHeader(
-      "Location",
-      `/public_page/wrong_url?wrong_slug=${slicedSlug}`
-    ); 
-    res.end()
-    return { props: null };
+    // res.statusCode = 302;
+    // res.setHeader(
+    //   "Location",
+    //   `/public_page/wrong_url?wrong_slug=${slicedSlug}`
+    // ); 
+    // res.end()
+    return { props: {isPublicPage: false} };
   }
 
   const returnData: IndexProps = {
