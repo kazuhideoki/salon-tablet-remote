af7842e1a597a98d8481b7df28c895ff44954853
Kazuhide Oki <cedar123pc@gmail.com>
2021-01-01 21:37:40 +0800

    refactor: IconAndText修正

--- src/app/View/tablet/Drawer/FooterItemEditor/components/iconSelect/icons.ts
+++ src/app/View/tablet/Drawer/FooterItemEditor/components/iconSelect/icons.ts
@@ -17,53 +17,53 @@ import {
   PlaceTwoTone,
   HomeTwoTone,
   Search,
-} from "@material-ui/icons";
-import { OverridableComponent } from "@material-ui/core/OverridableComponent";
-import { SvgIconTypeMap } from "@material-ui/core";
+} from '@material-ui/icons';
+import { OverridableComponent } from '@material-ui/core/OverridableComponent';
+import { SvgIconTypeMap } from '@material-ui/core';
 
 export const IconsSetting = class {
-         // static icons: any;
-         constructor() {}
+  // 単純な配列だとうまく行かなかったので各アイコンを配列に入れた
+  static icons: [
+    OverridableComponent<SvgIconTypeMap<Record<string, unknown>, 'svg'>>,
+    string
+  ][] = [
+    [Search, 'Search'],
+    [GradeTwoTone, 'GradeTwoTone'],
+    [FavoriteTwoTone, 'FavoriteTwoTone'],
+    [ListAltTwoTone, 'ListAltTwoTone'],
+    [FreeBreakfastTwoTone, 'FreeBreakfastTwoTone'],
+    [MenuBookTwoTone, 'MenuBookTwoTone'],
+    [ImportContactsTwoTone, 'ImportContactsTwoTone'],
+    [WifiTwoTone, 'WifiTwoTone'],
+    [ThumbUpAltTwoTone, 'ThumbUpAltTwoTone'],
+    [ChildCareTwoTone, 'ChildCareTwoTone'],
+    [PersonAddTwoTone, 'PersonAddTwoTone'],
+    [CameraAltTwoTone, 'CameraAltTwoTone'],
+    [PhotoSizeSelectActualTwoTone, 'PhotoSizeSelectActualTwoTone'],
+    [CommuteTwoTone, 'CommuteTwoTone'],
+    [PlaceTwoTone, 'PlaceTwoTone'],
+    [HomeTwoTone, 'HomeTwoTone'],
+    [LanguageTwoTone, 'LanguageTwoTone'],
+    [SettingsApplicationsTwoTone, 'SettingsApplicationsTwoTone'],
+  ];
 
-         // 単純な配列だとうまく行かなかったので各アイコンを配列に入れた
-         static icons: [
-           OverridableComponent<SvgIconTypeMap<{}, "svg">>,
-           string
-         ][] = [
-           [Search, "Search"],
-           [GradeTwoTone, "GradeTwoTone"],
-           [FavoriteTwoTone, "FavoriteTwoTone"],
-           [ListAltTwoTone, "ListAltTwoTone"],
-           [FreeBreakfastTwoTone, "FreeBreakfastTwoTone"],
-           [MenuBookTwoTone, "MenuBookTwoTone"],
-           [ImportContactsTwoTone, "ImportContactsTwoTone"],
-           [WifiTwoTone, "WifiTwoTone"],
-           [ThumbUpAltTwoTone, "ThumbUpAltTwoTone"],
-           [ChildCareTwoTone, "ChildCareTwoTone"],
-           [PersonAddTwoTone, "PersonAddTwoTone"],
-           [CameraAltTwoTone, "CameraAltTwoTone"],
-           [PhotoSizeSelectActualTwoTone, "PhotoSizeSelectActualTwoTone"],
-           [CommuteTwoTone, "CommuteTwoTone"],
-           [PlaceTwoTone, "PlaceTwoTone"],
-           [HomeTwoTone, "HomeTwoTone"],
-           [LanguageTwoTone, "LanguageTwoTone"],
-           [SettingsApplicationsTwoTone, "SettingsApplicationsTwoTone"],
-         ];
+  // アイコン名からもとの[IconComponent, IconName]の形に戻す。
+  static convertIconComponentFromName(
+    titleText: string
+  ):
+    | [
+        OverridableComponent<SvgIconTypeMap<Record<string, unknown>, 'svg'>>,
+        string
+      ]
+    | null {
+    const targetIcon = this.icons.find((value) => {
+      return value[1] == titleText;
+    });
 
-         // アイコン名からもとの[IconComponent, IconName]の形に戻す。
-         static convertIconComponentFromName(titleText: string) {
-           const targetIcon = this.icons.find((value) => {
-             return value[1] == titleText;
-           });
-
-           if (targetIcon) {
-             return targetIcon;
-           } else {
-             return null;
-           }
-         }
-       };
-
-
-
-
+    if (targetIcon) {
+      return targetIcon;
+    } else {
+      return null;
+    }
+  }
+};
