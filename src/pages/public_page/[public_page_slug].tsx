aebf09f92cccfde1811532a2c7e49281cbb7132d
Kazuhide Oki <cedar123pc@gmail.com>
2021-01-01 08:29:55 +0800

    fix: パブリックページの表示切り替えがうまく出来なかったので修正 #27

--- src/pages/public_page/[public_page_slug].tsx
+++ src/pages/public_page/[public_page_slug].tsx
@@ -6,18 +6,37 @@ import Head from 'next/head';
 import parser from "ua-parser-js";
 import { IndexProps } from '..';
 import { checkIsGeneratePubulicPage } from '../../lib/checkIsGeneratePubulicPage';
+import { makeStyles, Typography, Theme, createStyles } from "@material-ui/core";
 
-
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    typography: {
+      margin: theme.spacing(3),
+    },
+  })
+);
 
 const PublicPage = (props: IndexProps) => {
+  const classes = useStyles()
 
   if (props.isPublicPage === false) {
-    return (<>
-      <Head>
-        <meta name="robots" content="noindex" />
-      </Head>
-      <p>パブリックページが有効化されていません</p>
-    </>)
+    return (
+      <>
+        <Head>
+          <meta name="robots" content="noindex" />
+        </Head>
+        <Typography
+          className={classes.typography}
+          align="center"
+          variant="h4"
+          component="h2"
+          gutterBottom
+          color="textSecondary"
+        >
+          パブリックページが有効化されていません
+        </Typography>
+      </>
+    );
   }
 
   return (
