e2551fe93239b7aa5e1c427ea67b8871cbf6bdc1
Kazuhide Oki <cedar123pc@gmail.com>
2021-01-11 17:29:24 +0800

    productionもCD実装 #13

--- .github/scripts/decrypt_env.sh
+++ .github/scripts/decrypt_env.sh
@@ -5,3 +5,6 @@ gpg --quiet --batch --yes --decrypt --passphrase="$ENV_CREDENTIALS" \
 
 gpg --quiet --batch --yes --decrypt --passphrase="$ENV_CREDENTIALS" \
 --output .env.test .env.test.gpg
+
+gpg --quiet --batch --yes --decrypt --passphrase="$ENV_CREDENTIALS" \
+--output .env.production .env.production.gpg
