e3283c5251ba08c2fdd4315ecd490f8c9743d814
Kazuhide Oki <cedar123pc@gmail.com>
2021-01-11 16:28:49 +0800

    firebaseAdminsシークレットをファイルから読み込み

--- src/util/auth/firebaseAdmin.ts
+++ src/util/auth/firebaseAdmin.ts
@@ -1,11 +1,13 @@
 import * as firebaseAdmin from 'firebase-admin';
 
+import serviceAccount from '../../../firebase_secret.json';
+
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert({
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+      privateKey: serviceAccount.private_key,
+      clientEmail: serviceAccount.client_email,
+      projectId: serviceAccount.project_id,
     }),
     databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
   });
