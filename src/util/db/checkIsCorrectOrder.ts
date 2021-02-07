6fd6e3dad172952944963381d737462d65f5240a
Kazuhide Oki <cedar123pc@gmail.com>
2021-02-07 15:45:15 +0900

    checkIsCorrectOrderに名前変更

--- src/util/db/checkIsCorrectOrder.ts
+++ src/util/db/checkIsCorrectOrder.ts
@@ -1,6 +1,6 @@
 import { FooterItems } from '../interface/Interface';
 
-export const checkOrders = (footerItems: FooterItems): boolean => {
+export const checkIsCorrectOrder = (footerItems: FooterItems): boolean => {
   let result = true;
   footerItems.forEach((footerItem, index) => {
     // 昇順でDBから取得しているので、orderがindex+1でないと間違い
@@ -12,7 +12,9 @@ export const checkOrders = (footerItems: FooterItems): boolean => {
   return result;
 };
 
-export const checkOrdersSidebar = (footerItems: FooterItems): boolean => {
+export const checkIsCorrectOrderSidebar = (
+  footerItems: FooterItems
+): boolean => {
   let result = true;
   const footerItemsOnSidebar = footerItems.filter((value) => {
     return value.order_sidebar !== 0;
